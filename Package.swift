5ce232e9938dd8fcf46c5247367b75137d4fcec3
Daniel Inoa <danielinoa@gmail.com>
2026-08-02 23:31:59 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Folio",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Folio",
+            targets: ["Folio"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Folio"
+        ),
+        .testTarget(
+            name: "FolioTests",
+            dependencies: ["Folio"]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
